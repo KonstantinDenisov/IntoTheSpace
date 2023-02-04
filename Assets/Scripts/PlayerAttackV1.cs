80752338429f3a32128acfbde23caa839366adac
KonstantinDenisov <omacs@mail.ru>
2023-02-04 16:59:25 +0300

    Add LeanPool, bullet

---
+++ Assets/Scripts/PlayerAttackV1.cs
@@ -0,0 +1,54 @@
+﻿using Lean.Pool;
+using UnityEngine;
+
+public class PlayerAttackV1 : MonoBehaviour
+    {
+        #region Variables
+        
+        [SerializeField] private GameObject _bulletPrefab;
+        [SerializeField] private Transform _bulletSpawnPointTransform;
+        [SerializeField] private float _attackDelay;
+        private Transform _cachedTransform;
+        private float _delayTimer;
+
+        #endregion
+        
+        #region Unity Lifecycle
+
+        private void Awake()
+        {
+            _cachedTransform = transform;
+        }
+
+        private void Update()
+        {
+            TickTimer();
+
+            if (CanAttack())
+            {
+                Attack();
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool CanAttack()
+        {
+            return Input.GetButton("Fire1") && _delayTimer <= 0;
+        }
+
+        private void Attack()
+        {
+            LeanPool.Spawn(_bulletPrefab, _bulletSpawnPointTransform.position, _cachedTransform.rotation);
+            _delayTimer = _attackDelay;
+        }
+
+        private void TickTimer()
+        {
+            _delayTimer -= Time.deltaTime;
+        }
+
+        #endregion
+    }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovementService : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     #region Variables
 
