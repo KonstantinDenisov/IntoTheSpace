b4ca7623d646de92d931aa003e412c2b213a25e0
KonstantinDenisov <omacs@mail.ru>
2023-02-04 14:59:59 +0300

    Add Canvas, PauseService, PlayerMovementService.

---
+++ Assets/Scripts/PlayerMovementService.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerMovementService : MonoBehaviour
+{
+    #region Variables
+
+    private PauseService _pauseService;
+
+    #endregion
+
+
+    #region Unity LifeCycle
+
+    void Start()
+    {
+        _pauseService = FindObjectOfType<PauseService>();
+    }
+    
+    void Update()
+    {
+        if (_pauseService.IsPaused)
+            return;
+
+        else
+        {
+            Vector3 mousePositionInPixels = Input.mousePosition;
+            Vector3 mousePositionInUnits = Camera.main.ScreenToWorldPoint(mousePositionInPixels);
+            Vector3 currentPosition = transform.position;
+            currentPosition.x = mousePositionInUnits.x;
+            transform.position = currentPosition; 
+        } 
+    }
+
+    #endregion
+  
+}
