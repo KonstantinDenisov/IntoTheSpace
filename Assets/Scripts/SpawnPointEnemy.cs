e44fc13511a0f1a9d091349c94c07a4a662fc01f
KonstantinDenisov <omacs@mail.ru>
2023-02-06 21:44:43 +0300

    Create SpawnPointEnemy

---
+++ Assets/Scripts/SpawnPointEnemy.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class SpawnPointEnemy : MonoBehaviour
+{
+   [SerializeField] private float _timeEnemySpawn;
+   [SerializeField] private float _timeWaveSpawn;
+   
+   [SerializeField] private GameObject _enemyV1;
+   [SerializeField] private GameObject _enemyV2;
+   [SerializeField] private GameObject _enemyV3;
+   [SerializeField] private GameObject _enemyV4;
+   [SerializeField] private GameObject _enemyV5;
+   [SerializeField] private GameObject _enemyV6;
+   [SerializeField] private GameObject _enemyV7;
+   [SerializeField] private GameObject _enemyV8;
+
+   [SerializeField] private int _enemyV1Quantity;
+   [SerializeField] private int _enemyV2Quantity;
+   [SerializeField] private int _enemyV3Quantity;
+   [SerializeField] private int _enemyV4Quantity;
+   [SerializeField] private int _enemyV5Quantity;
+   [SerializeField] private int _enemyV6Quantity;
+   [SerializeField] private int _enemyV7Quantity;
+   [SerializeField] private int _enemyV8Quantity;
+
+
+   private void Start()
+   {
+      StartCoroutine(Spawn1());
+   }
+
+   private IEnumerator Spawn1()
+   {
+      for (int i = 0; i < _enemyV1Quantity; i++)
+      {
+         Instantiate(_enemyV1, transform.position, Quaternion.identity);
+         yield return new WaitForSeconds(_timeEnemySpawn);
+      }
+      
+   }
+}
