12435c641f01c102e27626bc7917a62b8d8840e5
KonstantinDenisov <omacs@mail.ru>
2023-03-01 20:38:58 +0300

    Renaming a variable OFF, Union BulletEnemy and BulletPlayer

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using Lean.Pool;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BulletPlayer : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private float _speed;
+    [SerializeField] private float _lifeTime;
+    [SerializeField] public int Damage;
+    [SerializeField] private bool _isEnemyBullet;
+    private Rigidbody2D _rb;
+
+    #endregion
+
+
+    #region Unity Lifecycle
+
+    private void Awake()
+    {
+        StartCoroutine(LifeTimeTimer());
+        _rb = GetComponent<Rigidbody2D>();
+        if (_isEnemyBullet)
+        {
+            _rb.velocity = transform.up * _speed * -1;
+        }
+        else
+        {
+            _rb.velocity = transform.up * _speed; 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (_isEnemyBullet)
+        {
+            if (col.CompareTag(Tags.Player))
+            {
+                PlayerHP playerHp = col.gameObject.GetComponentInParent<PlayerHP>();
+            
+                if (playerHp != null)
+                { 
+                    playerHp.ApplyDamage(Damage);
+                }
+            }
+        }
+
+        else
+        {
+            if (col.CompareTag(Tags.Enemy))
+            {
+                EnemyHP enemyHp = col.gameObject.GetComponentInParent<EnemyHP>();
+            
+                if (enemyHp != null)
+                { 
+                    enemyHp.ApplyDamage(Damage);
+                }
+            }
+        }
+    }
+
+    #endregion
+
+
+    #region Private Methods
+
+    private IEnumerator LifeTimeTimer()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+       Destroy(gameObject);
+       //Despawn();
+    }
+
+    private void Despawn()
+    {
+        LeanPool.Despawn(gameObject);
+    }
+
+    #endregion
+}
