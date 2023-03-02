60c5be3b4d3e19a66b02f3d1147786c658945908
KonstantinDenisov <omacs@mail.ru>
2023-03-02 22:30:15 +0300

    SpawnPointEnemy optimization with a mistake

--- Assets/Scripts/SpawnPointEnemy.cs
+++ Assets/Scripts/SpawnPointEnemy.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPointEnemy : MonoBehaviour
@@ -8,25 +9,11 @@ public class SpawnPointEnemy : MonoBehaviour
 
    [SerializeField] private float _timeEnemySpawn;
    [SerializeField] private float _timeWaveSpawn;
-   
-   [SerializeField] private GameObject _enemyV1;
-   [SerializeField] private GameObject _enemyV2;
-   [SerializeField] private GameObject _enemyV3;
-   [SerializeField] private GameObject _enemyV4;
-   [SerializeField] private GameObject _enemyV5;
-   [SerializeField] private GameObject _enemyV6;
-   [SerializeField] private GameObject _enemyV7;
-   [SerializeField] private GameObject _enemyV8;
 
-   [SerializeField] private int _enemyV1Quantity;
-   [SerializeField] private int _enemyV2Quantity;
-   [SerializeField] private int _enemyV3Quantity;
-   [SerializeField] private int _enemyV4Quantity;
-   [SerializeField] private int _enemyV5Quantity;
-   [SerializeField] private int _enemyV6Quantity;
-   [SerializeField] private int _enemyV7Quantity;
-   [SerializeField] private int _enemyV8Quantity;
-   
+   [SerializeField] private List <GameObject> _enemys;
+
+   [SerializeField] private List <int> _enemyQuantity;
+
    private GameWinScreenService _gameWinScreenService;
 
    #endregion
@@ -37,7 +24,7 @@ public class SpawnPointEnemy : MonoBehaviour
    {
       _gameWinScreenService = FindObjectOfType<GameWinScreenService>();
       
-      StartCoroutine(Spawn1());
+      StartCoroutine(FirstSpawn());
    }
 
    #endregion
@@ -45,108 +32,33 @@ public class SpawnPointEnemy : MonoBehaviour
 
    #region Private Methods
 
-   private IEnumerator Spawn1()
+   private IEnumerator FirstSpawn()
    {
       yield return new WaitForSeconds(5);
       
-      for (int i = 0; i < _enemyV1Quantity; i++)
+      for (int i = 0; i < _enemyQuantity[0]; i++)
       {
-         Instantiate(_enemyV1, transform.position, Quaternion.identity);
+         Instantiate(_enemys[0], transform.position, Quaternion.identity);
          yield return new WaitForSeconds(_timeEnemySpawn);
       }
 
-      StartCoroutine(Spawn2());
+      int numberOfWaves = _enemys.Count;
+
+      for (int i = 1; i < numberOfWaves; i++)
+      {
+         yield return new WaitForSeconds(_timeWaveSpawn);
+
+         StartCoroutine(NextSpawn(i));
+      }
    }
 
-   private IEnumerator Spawn2()
+   private IEnumerator NextSpawn(int currentWaveNumber)
    {
-      yield return new WaitForSeconds(_timeWaveSpawn);
-      
-      for (int i = 0; i < _enemyV2Quantity; i++)
+      for (int i = 0; i < _enemyQuantity[currentWaveNumber]; i++)
       {
-         Instantiate(_enemyV2, transform.position, Quaternion.identity);
+         Instantiate(_enemys[i], transform.position, Quaternion.identity);
          yield return new WaitForSeconds(_timeEnemySpawn);
       }
-      
-      StartCoroutine(Spawn3());
-   }
-   
-   private IEnumerator Spawn3()
-   {
-      yield return new WaitForSeconds(_timeWaveSpawn);
-      
-      for (int i = 0; i < _enemyV3Quantity; i++)
-      {
-         Instantiate(_enemyV3, transform.position, Quaternion.identity);
-         yield return new WaitForSeconds(_timeEnemySpawn);
-      }
-      
-      StartCoroutine(Spawn4());
-   }
-   
-   private IEnumerator Spawn4()
-   {
-      yield return new WaitForSeconds(_timeWaveSpawn);
-      
-      for (int i = 0; i < _enemyV4Quantity; i++)
-      {
-         Instantiate(_enemyV4, transform.position, Quaternion.identity);
-         yield return new WaitForSeconds(_timeEnemySpawn);
-      }
-      
-      StartCoroutine(Spawn5());
-   }
-   
-   private IEnumerator Spawn5()
-   {
-      yield return new WaitForSeconds(_timeWaveSpawn);
-      
-      for (int i = 0; i < _enemyV5Quantity; i++)
-      {
-         Instantiate(_enemyV5, transform.position, Quaternion.identity);
-         yield return new WaitForSeconds(_timeEnemySpawn);
-      }
-      
-      StartCoroutine(Spawn6());
-   }
-   private IEnumerator Spawn6()
-   {
-      yield return new WaitForSeconds(_timeWaveSpawn);
-      
-      for (int i = 0; i < _enemyV6Quantity; i++)
-      {
-         Instantiate(_enemyV6, transform.position, Quaternion.identity);
-         yield return new WaitForSeconds(_timeEnemySpawn);
-      }
-      
-      StartCoroutine(Spawn7());
-   }
-   
-   private IEnumerator Spawn7()
-   {
-      yield return new WaitForSeconds(_timeWaveSpawn);
-      
-      for (int i = 0; i < _enemyV7Quantity; i++)
-      {
-         Instantiate(_enemyV7, transform.position, Quaternion.identity);
-         yield return new WaitForSeconds(_timeEnemySpawn);
-      }
-      
-      StartCoroutine(Spawn8());
-   }
-   private IEnumerator Spawn8()
-   {
-      yield return new WaitForSeconds(_timeWaveSpawn);
-      
-      for (int i = 0; i < _enemyV8Quantity; i++)
-      {
-         Instantiate(_enemyV8, transform.position, Quaternion.identity);
-         yield return new WaitForSeconds(_timeEnemySpawn);
-      }
-      
-      yield return new WaitForSeconds(_timeWaveSpawn);
-      
-      _gameWinScreenService.GameWin();
    }
 
    #endregion
